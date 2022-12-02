7930ccad3f87c9dc788ed640636726cb178c2640
Shaheed-N <username@example.com>
2022-12-02 09:44:21 +0400

    Initial Commit

---
+++ ImageAI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageAI
+//
+//  Created by Shahid on 02.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageAI/ImageAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAIApp.swift
+//  ImageAI
+//
+//  Created by Shahid on 02.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ImageAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
